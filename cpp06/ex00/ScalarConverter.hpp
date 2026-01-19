cd13e2148dee9de386a977a35873c1f48a8242f6
samokhta <saeljadmokhtari@gmail.com>
2026-01-19 15:07:43 +0100

    wip cpp06

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,17 @@
+#ifndef SCALAR_HPP
+# define SCALAR_HPP
+# include <string>
+# include <iostream>
+
+	class ScalarConverter
+	{
+	private:
+		ScalarConverter(void);
+		ScalarConverter(const ScalarConverter& other);
+		ScalarConverter	&operator=(const ScalarConverter& other);
+		~ScalarConverter();
+	public:
+		static void	convert(std::string num);
+	};
+
+#endif
