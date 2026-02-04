632c3eaf789b4ed7c6fe83216318fef19c2615ba
Sael-Jad Mokhtari <samokhta@c2r2s3.42lausanne.ch>
2026-02-04 11:36:56 +0100

    ex01 done

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
 template<typename T>
-void	iter(T* array, int length, void (*f)(T &n))
+void	print(const T &n) { std::cout << n << std::endl; }
+template<typename T>
+void	incr(T &n) { n++; }
+
+template<typename T>
+void	iter(const T* array, const int length, void (*f)(const T &n))
 {
+	if (!array || !f || length <= 0)
+		return ;
 	for (int i = 0; i < length; i++)
-	{
 		f(array[i]);
-	}
+}
+
+template<typename T>
+void	iter(T* array, const int length, void (*f)(T &n))
+{
+	if (!array || !f || length <= 0)
+		return ;
+	for (int i = 0; i < length; i++)
+		f(array[i]);
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -2,5 +2,21 @@
 
 int	main()
 {
-	
+	int			a[5] = {0, 1, 2, 3, 4};
+	double		b[5] = {0.0, 1.1, 2.2, 3.3, 4.4};
+	char		c[5] = "test";
+	const char	d[5] = "test";
+
+	std::cout << "int test: " << std::endl;
+	iter(a, 5, incr);
+	iter(a, 5, print);
+	std::cout << "double test: " << std::endl;
+	iter(b, 5, incr);
+	iter(b, 5, print);
+	std::cout << "char test: " << std::endl;
+	iter(c, 5, incr);
+	iter(c, 5, print);
+	std::cout << "const char test: " << std::endl;
+	iter(d, 5, print);
+	return 0;
 }
