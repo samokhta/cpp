a888ba26619ccecbf69bc4a08fb7b670890542ba
samokhta <saeljadmokhtari@gmail.com>
2026-03-19 17:12:54 +0100

    wip ex02

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,21 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+#include <vector>
+#include <deque>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <ctime>
+#include <cstdlib>
+
+class PmergeMe
+{
+	private:
+		/* data */
+	public:
+		PmergeMe(/* args */);
+		~PmergeMe();
+};
+
+#endif
---
+++ cpp09/ex02/main.cpp
@@ -0,0 +1,11 @@
+#include "PmergeMe.hpp"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cerr << "Error: not enough arguments" << std::endl;
+		return 1;
+	}
+	return 0;
+}
